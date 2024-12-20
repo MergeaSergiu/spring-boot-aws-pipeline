4732dd7494400ded3dd1b049fcfa9ec826bcf978
MergeaSergiu <mergeasergiu@gmail.com>
2024-12-20 21:22:22 +0200

    Initial commit

---
+++ spring-boot-cicd/src/main/java/spring_boot_cicd/api/HelloWorldApi.java
@@ -0,0 +1,13 @@
+package spring_boot_cicd.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldApi {
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
